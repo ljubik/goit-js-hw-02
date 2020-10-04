92e6b9031f3844083abee086291e2732b689cce7
Liubomyr <ljubik@ukr.net>
2020-10-04 19:03:25 +0300

    make task#05 complete

--- js/task-05.js
+++ js/task-05.js
@@ -1,74 +1,24 @@
 // Завдання 5
-// Задача 1-5
-// Использование switch, if и методов slice, toUpperCase, toLowerCase
-// Напиши скрипт имитирующий оформление заказа в избранную страну.
+// Задача 2-5
+// Функция предикат
+// Функция предикат возвращает true или false
 
-// Пользователь может оформить доставку товара к себе в страну, указав ее в переменной countryName .
-// Учти, пользователь может ввести имя страны не только буквами нижнего регистра, а к примеру 'кИтАЙ'. Поэтому надо перезаписать текст переменной countryName так, чтобы первая буква (свойство [0]) была заглавной (метод toUpperCase), а остальные буквы(метод slice) были в нижнем регистре(метод toLowerCase).
+// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.
+// Функция проверяет ее на содержание слов spam и sale. Если нашли запрещенное слово то
+// функция возвращает true, если
+// запрещенных слов нет функция возвращает false. Слова в строке могут быть в произвольном регистре.
 
-// Переменная message будет выводить сообщение. В эту переменную надо будет записать одно из трех сообщений о результате:
+// Здано:
 
-// const CANCELED_BY_USER = 'Отменено пользователем!'
-// Доставка в ${} будет стоить ${} кредитов
-// const NO_DELIVERY = 'В выбранную страну доставка не доступна.
-// Для определения стоимости (переменная price) или записи сообщения о невозможности доставки используй switch.
-
-// В случае, если доставка возможна (цена больше 0), сформируй сообщение о стоимости доставки в указанную страну с помощью if.
-
-// Ниже приведен список стран и стоимость доставки.
-
-// Китай - 100 кредитов
-// Австралия - 170 кредитов
-// Индия - 80 кредитов
-// Ямайка - 120 кредитов
-
-const countryName = "ЯмайкА"
-
-const CANCELED_BY_USER = "Отменено пользователем!"
-const NO_DELIVERY = "В выбранную страну доставка недоступна."
-const CHINA = "Китай"
-const AUSTRALIA = "Австралия"
-const INDIA = "Индия"
-const JAMAICA = "Ямайка"
-let message
-let price = 0
-let country
-if (countryName === null) {
-	message = CANCELED_BY_USER
-} else {
-	country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase() // Write code on this line
-	switch (country) {
-		// Write code under this line
-		case CHINA:
-			price = 100
-			break
-		case AUSTRALIA:
-			price = 170
-			break
-		case INDIA:
-			price = 80
-			break
-		case JAMAICA:
-			price = 120
-			break
-		default:
-			message = NO_DELIVERY
-	}
-}
-if (price !== 0) {
-	message = `Доставка в ${country} будет стоить ${price} кредитов`
+let checkForSpam = function (str) {
+	let strForCheck = str.toLowerCase()
+	return strForCheck.includes(`spam`) || strForCheck.includes(`sale`)
 }
 
-console.log(message)
+console.log(checkForSpam("Latest technology news")) // false
 
-//если countryName равно "КитаЙ"
-// то значение message будет равно
-// 'Доставка в Китай будет стоить 100 кредитов'
+console.log(checkForSpam("JavaScript weekly newsletter")) // false
 
-//если countryName равно null
-// то значение message будет равно
-// 'Отменено пользователем!'
+console.log(checkForSpam("Get best sale offers now!")) // true
 
-//если countryName равно "Чили"
-// то значение message будет равно
-// 'В выбранную страну доставка недоступна.'
+console.log(checkForSpam("[SPAM] How to earn fast money?")) // true
